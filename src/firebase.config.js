c783ae4aa76ee94002398ce08a707c858ea80931
Br4z <67658575+Br4z@users.noreply.github.com>
2023-11-08 10:14:02 -0500

    Edit firebase configuration to access environment vars

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXEM5VehVq-_4LITXVnAAZCP1xYAEyTmc",
-    authDomain: "saekano-7f54b.firebaseapp.com",
-    projectId: "saekano-7f54b",
-    storageBucket: "saekano-7f54b.appspot.com",
-    messagingSenderId: "414290666454",
-    appId: "1:414290666454:web:b9967aaab6af47b02afa77",
-    measurementId: "G-NR06Y1LPP7"
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
