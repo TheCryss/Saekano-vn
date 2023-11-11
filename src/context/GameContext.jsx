3a237abfb08c0f87bd9d16f5862ece53e9d9e1a7
GetialJuan <getial.juan@correounivalle.edu.co>
2023-11-11 11:32:10 -0500

    GameContext added - Global actual scene - Global actual content - Methods for change the actual scene and content

---
+++ src/context/GameContext.jsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import data from '../script/scene_1.json'
+
+export const gameContext = createContext();
+
+export const useGame = () => {
+    const context = useContext(gameContext);
+    if (!context) {
+        console.log("Error creating game context");
+    }
+    return context;
+}
+
+export function GameProvider({ children }) {
+    const scenes = data.scenes;
+    const [gameState, setGameState] = useState({
+        actualScene: 0,
+        actualContentIndex: 0,
+        // Here you can add more game states
+    });
+
+    // This useEffect is for initialize the game state
+    // with the state of the user in db
+    // useEffect(() => {}, []);
+
+    const nextScene = () => {
+
+        const nextScene = gameState.actualScene + 1;
+        if (scenes[nextScene] == undefined) {
+            return { success: false, error: "No more scenes" }
+        };
+
+        setGameState({ ...gameState, actualScene: nextScene, actualContentIndex: 0 });
+        return { success: true, scene: nextScene };
+
+    }
+
+    const nextContent = () => {
+        const nextContent = gameState.actualContentIndex + 1;
+        if (scenes[gameState.actualScene].content[nextContent] == undefined) {
+            return { success: false, error: "No more contents" }
+        }
+        setGameState({ ...gameState, actualContentIndex: nextContent });
+        return { success: true, content: nextContent };
+    }
+
+    const getActualContent = () => {
+        const { actualScene, actualContentIndex } = gameState;
+        return scenes[actualScene].content[actualContentIndex];
+    }
+
+    return (
+        <gameContext.Provider value={{ getActualContent, nextScene, nextContent }}>
+            {children}
+        </gameContext.Provider>
+    );
+}
