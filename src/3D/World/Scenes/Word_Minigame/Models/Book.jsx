7a5cc839bb281ca418d3201651d3086bbee2e0fb
Jose Luis <38475424+TheCryss@users.noreply.github.com>
2023-11-15 10:20:25 -0500

    Minigame1 text and  text click events

--- src/3D/World/Scenes/Word_Minigame/Models/Book.jsx
+++ src/3D/World/Scenes/Word_Minigame/Models/Book.jsx
@@ -6,34 +6,46 @@ export const Book = (props) => {
     const { nodes, materials } = useGLTF("/assets/models/book/Book.glb");
     return (
         <group {...props} dispose={null}>
-            <group position={[0, 0.015, 0]} rotation={[2.88, 0, Math.PI]} >
+            <group position={[0, 0.015, 0]} rotation={[2.88, 0, Math.PI]}>
                 <mesh
                     castShadow
                     
-                    geometry={nodes.Cube001_1.geometry}
+                    geometry={nodes.Cube001.geometry}
                     material={materials.Cover}
-                >
-                    <Html transform occlude rotation={[-Math.PI/2, 0, Math.PI/2]}>
-                        <Poem />
-                    </Html>
-                </mesh>
+                />
                 <mesh
                     castShadow
-                    
-                    geometry={nodes.Cube001_2.geometry}
-                    // material={materials.Paper}
+                    // receiveShadow
+                    geometry={nodes.Cube001_1.geometry}
+                    material={materials.Paper}
                 >
-                    <meshStandardMaterial ></meshStandardMaterial>
+                    <Html position={[0,-.1,-0.55]} transform occlude  rotation-x={-Math.PI/2} rotation-z={Math.PI/2}>
+                        <Poem  />
+                    </Html>
                 </mesh>
             </group>
             <mesh
                 castShadow
                 
-                geometry={nodes.Cube.geometry}
+                geometry={nodes.Line_in_the_middle.geometry}
                 material={materials.Black}
                 position={[0, -0.097, 0]}
                 scale={[0.949, 0.01, 0.01]}
             />
+            <group position={[0, 0.015, 0]} rotation={[2.88, 0, Math.PI]}>
+                <mesh
+                    castShadow
+                    
+                    geometry={nodes.Cube003.geometry}
+                    material={materials.Cover}
+                />
+                <mesh
+                    castShadow
+                    
+                    geometry={nodes.Cube003_1.geometry}
+                    material={materials.Paper}
+                />
+            </group>
         </group>
     );
 
--- src/3D/World/Scenes/Word_Minigame/Text/Poem.jsx
+++ src/3D/World/Scenes/Word_Minigame/Text/Poem.jsx
@@ -2,13 +2,45 @@ import React from 'react'
 import './styles.css'
 
 export const Poem = () => {
+    const handleClick = (event) => {
+        const clickedWord = event.target.textContent;
+        console.log(`Se hizo clic en la palabra: ${clickedWord}`);
+    };
+
     return (
-        <div className='w-10  content'>
+        <div className='w-10  content' onClick={handleClick}>
             <div className='grid grid-cols-2 gap-1  '>
-                <p>Hola</p>
-                <p>Hellow</p>
-                <p>Hi</p>
-                <p>hodd</p>
+                <p>Heroina</p>
+                <p>Paisaje</p>
+                <p>Amor</p>
+                <p>Miedo</p>
+                <p>Profundo</p>
+                <p>familia</p>
+                <p>Susurro</p>
+                <p>Éxtasis</p>
+                <p>Melodía</p>
+                <p>Aurora</p>
+                <p>Esencia</p>
+                <p>Sinfonía</p>
+                <p>Serenidad</p>
+                <p>Destino</p>
+                <p>Quimera</p>
+                <p>Cielo</p>
+                {/* <p>Efímero</p>
+                <p>Encanto</p>
+                <p>Crepúsculo</p>
+                <p>Inefable</p>
+                <p>Anhelo</p>
+                <p>Lamento</p>
+                <p>Nostalgia</p>
+                <p>Perfume</p>
+                <p>Resplandor</p>
+                <p>Palpitar</p>
+                <p>Maravilla</p>
+                <p>Eclipse</p>
+                <p>Resplandor</p>
+                <p>Época</p>
+                <p>Soliloquio</p> */}
             </div>
         </div>
     )
