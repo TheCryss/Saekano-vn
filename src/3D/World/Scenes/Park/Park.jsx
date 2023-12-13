7d8209c148ddfb78155eb2c7f9a8ed4f99714615
TheCryss <38475424+TheCryss@users.noreply.github.com>
2023-12-13 00:50:52 -0500

    scripts fixed

--- src/3D/World/Scenes/Park/Park.jsx
+++ src/3D/World/Scenes/Park/Park.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Park3D } from './Models/Park'
 import { Lights_Room } from '../../Staging/Lights'
+import { Megumi } from './Models/Megumi'
+import { Utaha } from './Models/Utaha'
+import { Eriri } from './Models/Eriri'
 
 
 const Park = () => {
     return (
         <>
-            <Park3D position={[0,-5,0]} scale={0.08}/>
+            <Park3D position={[0,-7,0]} scale={0.08}/>
+            <Utaha scale={2} position={[0, 1, 0]} />
+            <Eriri scale={2} position={[4.9, 0.7, 27]} />
+            <Megumi scale={2} position={[-6.9, 0, 7]} rotation-y={Math.PI / 2} />
             <Lights_Room />
         </>
     )
--- src/Components/Choices2D.jsx
+++ src/Components/Choices2D.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 
 const choices = [
-    ["Manga", "Novela Ligera", "Salida de Baño"],
-    ["Manga", "Novela Ligera", "Salida de Baño"],
+    [],
+    ["Manga", "Salida de Baño", "Novela Ligera"],
     ["", "", ""],
     ["Eriri", "Megumi", "Utaha"],
     ["", "", ""],
@@ -28,13 +28,13 @@ const Choices2D = () => {
     return (
         <div className="absolute w-full min-h-screen flex items-center justify-center">
             <div className='  z-20 w-1/6 text-md  h-40 flex flex-col  '>
-                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation - 1, "bifurcationOption": 0 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)]  hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
+                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation, "bifurcationOption": 0 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)]  hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
                     {choices[bifurcation - 1][0]}
                 </button>
-                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation - 1, "bifurcationOption": 1 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)] hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
+                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation , "bifurcationOption": 1 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)] hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
                     {choices[bifurcation - 1][2]}
                 </button>
-                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation - 1, "bifurcationOption": 2 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)] hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
+                <button onClick={() => { dispatch(setPlayerBifurcation({ "bifurcationNumber": bifurcation , "bifurcationOption": 2 })); setEnd(true) }} className={`border-[#C6F5EB] text-pink-950 border-4 select-none my-2 flex items-center justify-center  bg-[rgba(242,198,245,0.75)] hover:bg-[#F5EAC6] hover:scale-105 hover:  transition-all  rounded-md py-1 font-bold `} type='button'>
                     {choices[bifurcation - 1][1]}
                 </button>
             </div>
